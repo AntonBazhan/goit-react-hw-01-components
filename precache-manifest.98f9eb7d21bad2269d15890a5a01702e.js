2d02a446e3751efb885e362bf70416a59d5decb7
Anton <bazhan.anton@ukr.net>
2020-06-06 01:40:01 +0300

    Updates

---
+++ precache-manifest.98f9eb7d21bad2269d15890a5a01702e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996f36ce92b9da8ce18331be8a2c249a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "12487296ae1dbde01bbb",
+    "url": "/goit-react-hw-01-components/static/css/main.aecdbfa0.chunk.css"
+  },
+  {
+    "revision": "b4254a951c4e5f6e185c",
+    "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.8f027ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12487296ae1dbde01bbb",
+    "url": "/goit-react-hw-01-components/static/js/main.cae55c44.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
